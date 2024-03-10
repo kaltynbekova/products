ebcb820024c025e0cd34c527ee2f36733a8c7570
kaltynbekova <kamilya.altynbekova@gmail.com>
2024-03-10 12:23:09 +0100

    Added models, basic view/template

--- product_search/models.py
+++ product_search/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+class Tag(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+class Product(models.Model):
+	title = models.CharField(max_length=100)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	tags = models.ManyToManyField(Tag, blank=True)
+
+	def __str__(self):
+		return self.title
--- product_search/views.py
+++ product_search/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def index(request):
+	products = Product.objects.all()
+	query_string = request.GET['search_string'];
+	if query_string:
+		products = products.filter(title__icontains=query_string)
+	return render(request, 'index.html', {'products': products})
