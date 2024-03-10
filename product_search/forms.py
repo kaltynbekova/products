eda97141728e641274eee32bb470c492693a4dc7
kaltynbekova <kamilya.altynbekova@gmail.com>
2024-03-10 12:53:56 +0100

    Refactored search form with crispy-forms

--- product_search/forms.py
+++ product_search/forms.py
@@ -1,7 +1,27 @@
 from django import forms
 from .models import Category, Tag, Product
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout,Submit
+from crispy_forms.bootstrap import InlineCheckboxes
 
 class ProductSearchForm(forms.Form):
 	search_string = forms.CharField(label='Search', max_length=100, required=False)
 	category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
-	tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), required=False)
+	tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple, required=False)
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.helper = FormHelper()
+
+		self.helper.form_id = 'search_form'
+		self.helper.form_class = 'search-filter-form mb-4'
+		self.helper.form_method = 'GET'
+		self.helper.use_custom_control = True 
+
+		self.helper.add_input(Submit('submit', 'Submit', css_class='btn btn-dark mr-3'))
+
+		self.helper.layout = Layout(
+			'search_string', 
+			'category',
+			InlineCheckboxes('tags', css_class="tag-checkboxes")
+		)
